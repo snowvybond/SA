a129a2cf1e7f7033b6939b86dfd5cb9d90fbb6b9
BenBenjaminTennyson <Benjamin10>
2017-11-25 17:50:08 +0700

    update report

--- src/report/Controller.java
+++ src/report/Controller.java
@@ -1,20 +1,55 @@
 package report;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import menu.ControllerCivil;
-
+import model.DatabaseConnecter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Controller {
+
+    @FXML protected TableColumn<TableReport , String> typeCarColumn;
+    @FXML protected TableColumn<TableReport , String> licensedPlateColumn;
+    @FXML protected TableColumn<TableReport , String> brandCarColumn;
+    @FXML protected TableColumn<TableReport , String> modelCarColumn;
+    @FXML protected TableColumn<TableReport , String> totalHourColumn;
+    @FXML protected TableColumn<TableReport , String> totalDistanceColumn;
+    @FXML protected TableView<TableReport> table;
+    @FXML protected Label hourText;
+    @FXML protected Label distanceText;
+
+
     private Stage stage;
 
     public void setStage(Stage stage) { this.stage = stage; }
 
     public void handleBtnBackClickAction(){
         showMenu();
+    }
+
+
+
+
+    private void displayTable(String query){
+        ArrayList<ArrayList> allData = DatabaseConnecter.browseRequestForCar(query);
+        int count = 0;
+        for (ArrayList<String> i : allData){
+            table.getItems().add(count++,new TableReport(i.get(0),i.get(1),i.get(2),i.get(3),i.get(4) , i.get(5)));
+            typeCarColumn.setCellValueFactory(new PropertyValueFactory<>("typeCar"));
+            licensedPlateColumn.setCellValueFactory(new PropertyValueFactory<>("licensedPlate"));
+            brandCarColumn.setCellValueFactory(new PropertyValueFactory<>("brandCar"));
+            modelCarColumn.setCellValueFactory(new PropertyValueFactory<>("genCar"));
+            totalHourColumn.setCellValueFactory(new PropertyValueFactory<>("totalHour"));
+            totalDistanceColumn.setCellValueFactory(new PropertyValueFactory<>("totalDistance"));
+            hourText.setText((Integer.parseInt(hourText.getText()) + Integer.parseInt(i.get(4)))+"");
+            distanceText.setText((Integer.parseInt(distanceText.getText()) + Integer.parseInt(i.get(5)))+"");
+        }
     }
 
     private void showMenu() {
--- src/report/TableReport.java
+++ src/report/TableReport.java
@@ -1,4 +1,39 @@
 package report;
 
 public class TableReport {
+
+    private String typeCar;
+    private String licensedPlate;
+    private String brandCar;
+    private String genCar;
+    private String totalHour;
+    private String totalDistance;
+
+    public TableReport(String typeCar, String licensedPlate, String brandCar, String genCar, String totalHour, String totalDistance) {
+        this.typeCar = typeCar;
+        this.licensedPlate = licensedPlate;
+        this.brandCar = brandCar;
+        this.genCar = genCar;
+        this.totalHour = totalHour;
+        this.totalDistance = totalDistance;
+    }
+
+    public String getTypeCar() {
+        return typeCar;
+    }
+    public String getLicensedPlate() {
+        return licensedPlate;
+    }
+    public String getBrandCar() {
+        return brandCar;
+    }
+    public String getGenCar() {
+        return genCar;
+    }
+    public String getTotalHour() {
+        return totalHour;
+    }
+    public String getTotalDistance() {
+        return totalDistance;
+    }
 }
